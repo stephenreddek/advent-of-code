d952f08eccc898ca160de47dc418409db42077e1
Stephen Reddekopp <stephen.reddekopp@gmail.com>
2020-12-07 09:53:56 -0600

    days 5 and 6 working solutions

--- src/AdventOfCode2020/Day05.elm
+++ src/AdventOfCode2020/Day05.elm
@@ -2,6 +2,7 @@ module AdventOfCode2020.Day05 exposing (part1, part2)
 
 -- Imports ---------------------------------------------------------------------
 import Basics.Extra as Basics
+import List.Extra
 import Parser exposing (Parser, Trailing (..), Step (..), (|.), (|=))
 
 -- Types -----------------------------------------------------------------------
@@ -9,23 +10,83 @@ import Parser exposing (Parser, Trailing (..), Step (..), (|.), (|=))
 
 -- Input -----------------------------------------------------------------------
 --
-parseInput : String -> Result String Int
+parseInput : String -> Result String (List String)
 parseInput input =
-  Err "I don't parse any input right now!"
+  Parser.run inputParser input
+      |> Result.mapError Parser.deadEndsToString
 
 --
-inputParser : Parser ()
+inputParser : Parser (List String)
 inputParser =
-  Parser.succeed ()
+  Parser.loop [] <| (\xs ->
+      Parser.oneOf
+        [ Parser.succeed (\_ -> Done (List.reverse xs))
+            |= Parser.end
+        , Parser.succeed (\x -> Loop (x :: xs))
+            |= Parser.getChompedString (Parser.chompUntil "\n")
+            |. Parser.spaces
+        ]
+   )
 
 -- Functions -------------------------------------------------------------------
+calculateSeat : String -> Int
+calculateSeat positionText =
+    let
+        rowPart =
+            String.left 7 positionText
 
+        columnPart =
+            String.dropLeft 7 positionText
+    in
+    (calculateRow rowPart) * 8 + (calculateColumn columnPart)
+
+calculateRow : String -> Int
+calculateRow rowText =
+    calculateRowHelp 0 128 rowText
+
+calculateRowHelp : Int -> Int -> String -> Int
+calculateRowHelp acc step rowText =
+    case String.uncons rowText of
+        Just ('F', rest) ->
+            calculateRowHelp acc (step // 2) rest
+
+        Just ('B', rest) ->
+            calculateRowHelp (acc + (step // 2)) (step // 2) rest
+
+        _ ->
+            acc
+
+
+calculateColumn : String -> Int
+calculateColumn columnText =
+    calculateColumnHelp 0 8 columnText
+
+
+calculateColumnHelp : Int -> Int -> String -> Int
+calculateColumnHelp acc step columnText =
+    case String.uncons columnText of
+        Just ('L', rest) ->
+            calculateColumnHelp acc (step // 2) rest
+
+        Just ('R', rest) ->
+            calculateColumnHelp (acc + (step // 2)) (step // 2) rest
+
+        _ ->
+            acc
+
+
+findHole : List Int -> Maybe Int
+findHole spots =
+    List.foldl (\position (prev, found) -> if position == prev + 2 then (position, Just (position - 1)) else (position, found) ) (1000,Nothing) spots
+        |> Tuple.second
 
 -- Solvers ---------------------------------------------------------------------
 part1 : String -> Result String Int
 part1 input =
-  parseInput input
+    parseInput input
+        |> Result.map (List.map calculateSeat >> List.maximum >> Maybe.withDefault 0)
 
 part2 : String -> Result String Int
 part2 input =
   parseInput input
+    |> Result.map (List.map calculateSeat >> List.sort >> findHole >> Maybe.withDefault 0)
--- src/AdventOfCode2020/Day06.elm
+++ src/AdventOfCode2020/Day06.elm
@@ -3,29 +3,69 @@ module AdventOfCode2020.Day06 exposing (part1, part2)
 -- Imports ---------------------------------------------------------------------
 import Basics.Extra as Basics
 import Parser exposing (Parser, Trailing (..), Step (..), (|.), (|=))
+import Set
 
 -- Types -----------------------------------------------------------------------
 
 
 -- Input -----------------------------------------------------------------------
 --
-parseInput : String -> Result String Int
+parseInput : String -> Result String (List (List String))
 parseInput input =
-  Err "I don't parse any input right now!"
+  Parser.run inputParser input
+      |> Result.mapError Parser.deadEndsToString
 
 --
-inputParser : Parser ()
+inputParser : Parser (List (List String))
 inputParser =
-  Parser.succeed ()
+  Parser.loop [] <| (\xs ->
+      Parser.oneOf
+        [ Parser.succeed (\_ -> Done (List.reverse xs))
+            |= Parser.end
+        , Parser.succeed (\x -> Loop (x :: xs))
+            |= groupParser
+        ]
+   )
+
+
+groupParser : Parser (List String)
+groupParser =
+    Parser.loop [] <| (\xs ->
+          Parser.oneOf
+            [ Parser.succeed (Done (List.reverse xs))
+                 |. Parser.token "\n"
+            , Parser.succeed (\_ -> Done (List.reverse xs))
+                |= Parser.end
+            , Parser.succeed (\x -> Loop (x :: xs))
+                |= (Parser.getChompedString (Parser.chompUntil "\n"))
+                |. Parser.token "\n"
+            ]
+       )
 
 -- Functions -------------------------------------------------------------------
+numberOfAnyPositives : List String -> Int
+numberOfAnyPositives group =
+    List.foldl (\memberAnswer groupAnswer -> Set.union groupAnswer (Set.fromList (String.toList memberAnswer))) Set.empty group
+        |> Set.size
 
+numberOfAllPositives : List String -> Int
+numberOfAllPositives group =
+    let
+        startingSet =
+            List.head group
+                |> Maybe.map (String.toList >> Set.fromList)
+                |> Maybe.withDefault Set.empty
+    in
+    List.foldl (\memberAnswer groupAnswer -> Set.intersect groupAnswer (Set.fromList (String.toList memberAnswer))) startingSet group
+        |> Set.size
 
 -- Solvers ---------------------------------------------------------------------
 part1 : String -> Result String Int
 part1 input =
-  parseInput input
+    parseInput input
+        |> Result.map (List.map numberOfAnyPositives >> List.sum)
 
 part2 : String -> Result String Int
 part2 input =
-  parseInput input
+    parseInput input
+        |> Result.map (List.map numberOfAllPositives >> List.sum)
