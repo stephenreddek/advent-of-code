f1b0fef690a71a7ac2c0840932b4f24cd2f5fbf8
Stephen Reddekopp <stephen.reddekopp@gmail.com>
2020-12-09 09:06:09 -0600

    add day 9 part 2 solution

--- src/AdventOfCode2020/Day09.elm
+++ src/AdventOfCode2020/Day09.elm
@@ -3,6 +3,7 @@ module AdventOfCode2020.Day09 exposing (part1, part2)
 -- Imports ---------------------------------------------------------------------
 import Basics.Extra as Basics
 import List.Extra
+import Maybe.Extra
 import Parser exposing (Parser, Trailing (..), Step (..), (|.), (|=))
 import Util.Parser
 
@@ -59,6 +60,87 @@ failsTest preamble toEvaluate =
         |> List.any (\(x, y) -> x + y == toEvaluate)
         |> not
 
+
+solvePart2 : Int -> List Int -> Result String Int
+solvePart2 sumToFind numbers =
+    mapFind (\startingIndex -> contiguousNumbersThatSumTo sumToFind (List.drop startingIndex numbers)) (List.range 0 (List.length numbers - 1))
+        |> Result.fromMaybe "cannot find contiguous numbers"
+        |> Result.andThen sumSmallestAndLargest
+
+
+mapFind : (a -> Maybe b) -> List a -> Maybe b
+mapFind mapper list =
+    case list of
+        [] ->
+            Nothing
+
+        first :: rest ->
+            let
+                mapped =
+                    mapper first
+            in
+            case mapped of
+                Just value ->
+                    Just value
+
+                Nothing ->
+                    mapFind mapper rest
+
+
+foldUntilResult : (a -> b -> (b, Maybe c)) -> b -> List a -> Maybe c
+foldUntilResult func acc list =
+    case list of
+        [] ->
+            Nothing
+
+        x :: xs ->
+            case func x acc of
+                ( _, Just value ) ->
+                    Just value
+
+                ( newAcc, Nothing ) ->
+                    foldUntilResult func newAcc xs
+
+
+
+contiguousNumbersThatSumTo : Int -> List Int -> Maybe (List Int)
+contiguousNumbersThatSumTo sumToFind allNumbers =
+    foldUntilResult (\nextNum previousNumbers ->
+            let
+                currentSum =
+                    List.sum (nextNum :: previousNumbers)
+            in
+            if currentSum > sumToFind then
+                (previousNumbers, Just Nothing)
+
+            else if currentSum == sumToFind then
+                (nextNum :: previousNumbers, Just (Just (nextNum :: previousNumbers)))
+
+            else
+                (nextNum :: previousNumbers, Nothing)
+        )
+        []
+        allNumbers
+            |> Maybe.andThen identity
+
+
+rangeSumTo : Int -> List Int -> Bool
+rangeSumTo sumToCheck ints =
+    List.sum ints == sumToCheck
+
+
+sumSmallestAndLargest : List Int -> Result String Int
+sumSmallestAndLargest ints =
+    let
+        smallest =
+            List.minimum ints
+
+        largest =
+            List.maximum ints
+    in
+    Maybe.map2 (+) smallest largest
+        |> Result.fromMaybe "Cannot sum smallest and largest in empty list"
+
 -- Solvers ---------------------------------------------------------------------
 part1 : String -> Result String Int
 part1 input =
@@ -68,4 +150,4 @@ part1 input =
 part2 : String -> Result String Int
 part2 input =
     parseInput input
-        |> Result.map (always 0)
+        |> Result.andThen (solvePart2 257342611)
