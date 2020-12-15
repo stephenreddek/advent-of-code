4a772acc507299e4bf70366bf61088ccebf7207e
Stephen Reddekopp <stephen.reddekopp@gmail.com>
2020-12-15 09:17:38 -0600

    day 15 solutions

--- src/AdventOfCode2020/Day15.elm
+++ src/AdventOfCode2020/Day15.elm
@@ -2,43 +2,73 @@ module AdventOfCode2020.Day15 exposing (part1, part2)
 
 -- Imports ---------------------------------------------------------------------
 import Basics.Extra as Basics
+import Dict exposing (Dict)
+import List.Extra
 import Parser exposing (Parser, Trailing (..), Step (..), (|.), (|=))
 import Util.Parser
 
 -- Types -----------------------------------------------------------------------
-
+type alias GameState =
+    { lastRoundPlayed : Int
+    , lastNumberSpoken : Int
+    , memory : Dict Int Int
+    }
 
 -- Input -----------------------------------------------------------------------
 --
-parseInput : String -> Result String (List String)
+parseInput : String -> Result String (List Int)
 parseInput input =
-    Parser.run inputParser input
-        |> Result.map (identity)
-        |> Result.mapError Util.Parser.deadEndsToString
-
---
-inputParser : Parser (List String)
-inputParser =
-    Parser.loop [] (\xs ->
-        Parser.oneOf
-            [ Parser.succeed (Done (List.reverse xs))
-              |. Parser.end
-            , Parser.succeed (\x -> Loop (x :: xs))
-                |= Parser.getChompedString (Parser.chompUntil "\n")
-                |. Parser.token "\n"
-            ]
-   )
+    Ok [16,1,0,18,12,14,19]
 
 -- Functions -------------------------------------------------------------------
+playGame : Int -> List Int -> Int
+playGame roundsToPlay startingList =
+    numberSpokenForRound roundsToPlay (setupGameState startingList)
+
+
+setupGameState : List Int -> GameState
+setupGameState startingList =
+    { lastRoundPlayed = List.length startingList
+    , lastNumberSpoken = Maybe.withDefault 0 (List.Extra.last startingList)
+    , memory = List.Extra.indexedFoldl (\index num -> Dict.insert num (index + 1)) Dict.empty startingList
+    }
+
+
+numberSpokenForRound : Int -> GameState -> Int
+numberSpokenForRound roundsToPlay ({ lastRoundPlayed, lastNumberSpoken, memory } as gameState) =
+    if roundsToPlay == lastRoundPlayed then
+        lastNumberSpoken
+    else
+        numberSpokenForRound roundsToPlay (playRound gameState)
+
+
+playRound : GameState -> GameState
+playRound gameState =
+    let
+        thisRound =
+            gameState.lastRoundPlayed + 1
+
+        thisNumber =
+            case Dict.get gameState.lastNumberSpoken gameState.memory of
+                Just roundItWasSpoken ->
+                    gameState.lastRoundPlayed - roundItWasSpoken
+
+                Nothing ->
+                    0
+    in
+    { lastRoundPlayed = thisRound
+    , lastNumberSpoken = thisNumber
+    , memory = Dict.insert gameState.lastNumberSpoken gameState.lastRoundPlayed gameState.memory
+    }
 
 
 -- Solvers ---------------------------------------------------------------------
 part1 : String -> Result String Int
 part1 input =
     parseInput input
-        |> Result.map (always 0)
+        |> Result.map (playGame 2020)
 
 part2 : String -> Result String Int
 part2 input =
     parseInput input
-        |> Result.map (always 0)
+        |> Result.map (playGame 30000000)
